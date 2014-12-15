d5e8cc0051ab416ede20d4762e8feff5e34f2271
Emil Vatai <emil.vatai@gmail.com>
2014-12-15 15:03:39 +0100

    4m ell update

--- 4m/prolog/4m-ell2-3.pl
+++ 4m/prolog/4m-ell2-3.pl
@@ -26,6 +26,15 @@
 
 % 1. Feladat twice/2 megírása: http://www.learnprolognow.org/lpnpage.php?pagetype=html&pageid=lpn-htmlse16
 % Megoldás: https://github.com/vatai/bolyai-org/blob/master/4m/prolog/listex.pl
+% twice([1,2,x,brek],X) -> X=[1,1,2,2,x,x,brek,brek]
+twice( [], [] ).
+twice( [H1|F1], [H1,H1|F2] ) :- twice(F1,F2).
+
+
+% [1,2,3] -> [3,6,9]
+haromszor([],[]).
+haromszor([H1|T1],[H2|T2] ) :- H2 is 3*H1, haromszor(T1,T2).
+
 
 
 % 2. mint a "hossz" list + rekurzió -> szám (ld LPN).
@@ -34,9 +43,9 @@ h([_|T],N) :- h(T,X), N is X+1.
 
 % szumma
 s([],0).
-s([H|T], S) :- s(T,R), S is R+H.
+s([H|T], N) :- s(T,X), N is X+H.
 
-% 3. ugyan ez akumulátorral.
+% 3. ugyan ez (hossz, majd szumma) akumulátorral.
 accLen([_|T],A,L)  :-    Anew  is  A+1,  accLen(T,Anew,L).
 accLen([],A,A). 
 accLen(L,N) :- accLen(L,0,N).
@@ -47,11 +56,27 @@ accsum(L,S) :- accsum(L,0,S).
 
 % 
 % 4. feladat max/3
+accMax([H|T],A,Max)  :-
+    H  >  A,
+    accMax(T,H,Max).
+
+accMax([H|T],A,Max)  :-
+    H  =<  A,
+    accMax(T,A,Max).
+   
+accMax([],A,A). 
+
 
 
 % 5. Írjuk meg a gl(N,L) predikátumot amely legenerálja az N hosszú
 % listát amely növekvő sorrendben tartalmazza az N legkisebb nem
 % negatív páratlan számot, azaz gl(3,L). esetén L=[1,3,5]
 
+
 gl(0,A,A).
-gl(N,A,R) :- N > 0, H is 2*N+1, Anew = [H|A], Nnew is N-1, gl(Nnew, Anew, R).
+gl(N,A,R) :- 
+    N > 0,
+    H is 2*N-1, 
+    Anew = [H|A], 
+    Nnew is N-1, 
+    gl(Nnew, Anew, R).
