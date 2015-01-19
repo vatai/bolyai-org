c7956777d352bf7ed7fe1149c50e43ccf7634356
Emil Vatai <emil.vatai@gmail.com>
2015-01-19 14:53:52 +0100

    2m lancolt lista.

--- 2m/pascal/llist.pas
+++ 2m/pascal/llist.pas
@@ -4,18 +4,39 @@ type
 	       value : integer;
 	       next  : ^node_t;
 	    end;     
-procedure writelist(l :^node_t);
+   list_t = ^node_t;
+
+procedure writelist(a : list_t);
 begin
+   while a <> nil do begin
+      writeln(a^.value);
+      a := a^.next;
+   end;
+end;
+
+procedure insert(a : list_t; v : integer);
+begin
+   { HF : az insert beszur egy 'v' értékű element az 'a' lista
+elejére! } 
 end;
 
 var
-   lst : ^node_t;
+   lst : list_t;
 begin
    writeln('hello');
+
    new(lst);
-   lst^.value := 3;
+   lst^.value := 42;
+
    new(lst^.next);
-   lst^.next^.value := 5;
-   lst^.next^.next := nil;
+   lst^.next^.value := 21;
+
+   new(lst^.next^.next);
+   lst^.next^.next^.value := 7;
+
+   lst^.next^.next^.next := nil;
+
    writelist(lst);
+   // insert(lst, 100);
+   // writelist(lst); // 100, 42, 21, 7
 end.
