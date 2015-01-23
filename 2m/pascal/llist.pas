297e58e4d1fe48197247f0c8ccbeba25eaca03d2
Emil Vatai <emil.vatai@gmail.com>
2015-01-23 12:01:03 +0100

    listak bovitve

--- 2m/pascal/llist.pas
+++ 2m/pascal/llist.pas
@@ -14,29 +14,97 @@ begin
    end;
 end;
 
-procedure insert(a : list_t; v : integer);
+procedure insert(var a : list_t; v : integer);
+var
+   tmp : list_t; { ^node_t} 
 begin
-   { HF : az insert beszur egy 'v' értékű element az 'a' lista
-elejére! } 
+   new(tmp);
+   tmp^.value := v;
+   tmp^.next := a;
+   a := tmp;
+end;
+
+procedure append(var a,b : list_t);
+begin
+   if a = nil then begin 
+      a:=b
+   end else begin
+      while a^.next <> nil do a := a^.next;
+      { a^.next = nil }
+      a^.next := b;
+   end;
+end;
+
+function append2(a,b : list_t) : list_t;
+begin
+   append2 := a;
+   if a = nil then begin
+      append2 := b;
+   end else begin
+      while a^.next <> nil do a := a^.next;
+      { a^.next = nil }
+      a^.next := b;
+      { append2 := ???? }
+   end;
+end;
+
+procedure delete(var a : list_t; v : integer);
+var
+   tmp : list_t;
+begin
+   if a^.value <> v then begin
+      while a^.next^.value <> v do 
+	 a := a^.next;
+      tmp := a^.next;
+      a^.next := tmp^.next;
+      dispose(tmp);
+   end else begin
+      tmp := a^.next;
+      dispose(a);
+      a := tmp;
+   end;
+end;
+
+function find(a	: list_t; v : integer): list_t;
+{ vissza adja a pointert az elso 'v' erteku node_t-re }
+{ n := find(lst, 55); // utana n^.value = 55 }
+{ writeln( find(lst, 55)^.value );  }
+begin
+   if a <> nil then begin
+      while a^.value <> v do a := a^.next;
+      { a^.value = v }
+      find := a;
+   end else begin
+      find := nil;
+   end;
 end;
 
 var
-   lst : list_t;
+   l1,l2 : list_t;
+   tmp	 : node_t;
 begin
    writeln('hello');
 
-   new(lst);
-   lst^.value := 42;
+   l1 := nil;
+   insert(l1, 10);
+   insert(l1, 20);
+   insert(l1, 30);
+   writelist(l1); // 100, 42, 21, 7
 
-   new(lst^.next);
-   lst^.next^.value := 21;
+   l2 := nil;
+   insert(l2,100);
+   insert(l2,200);
+   insert(l2,300);
+   writelist(l2);
+   writeln('----');
 
-   new(lst^.next^.next);
-   lst^.next^.next^.value := 7;
+   // append(l1,l2);
+   // l1 := append2(l1,l2);
+   // writelist(l1); // 30, 20, 10, 300, 200, 100
 
-   lst^.next^.next^.next := nil;
-
-   writelist(lst);
-   // insert(lst, 100);
-   // writelist(lst); // 100, 42, 21, 7
+   
+   // tmp := find(l2,200)^ {<<-- itt a kalap!!!} ;
+   // writeln(tmp.value);
+   delete(l1,30);
+   writelist(l1);
 end.
