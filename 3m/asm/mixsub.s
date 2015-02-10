84e927a5d30c12adb77671c47060d4252bd3d4ba
Emil Vatai <emil.vatai@gmail.com>
2015-02-10 12:37:38 +0100

    kommentek mixsub.s-ben.

--- 3m/asm/mixsub.s
+++ 3m/asm/mixsub.s
@@ -1,4 +1,4 @@
-	section	.text
-	global	fun
-fun:	mov	ax,13
-	ret
+	section	.text		; "Kód szegmens"
+	global	fun		; a fun cimke kívűről is hívható
+fun:	mov	ax,13 		; AX-be megy a visszatérési érték
+	ret			; "return"
