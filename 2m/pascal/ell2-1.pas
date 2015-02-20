6f15567f70dfce290afd1cec314e8f0de664c91d
Emil Vatai <emil.vatai@gmail.com>
2015-02-20 12:13:59 +0100

    2m ellenőrző feladatok vázlata.

---
+++ 2m/pascal/ell2-1.pas
@@ -0,0 +1,21 @@
+{ 
+
+1. Lista alap műveletek! Insert, delete, find stb.  
+
+Például: deleteEven(lst) az lst listából kitörli a páros számokat.
+
+2. Prioritásos lista: mindig rendezett.  
+
+Például: írjunk egy programot amely beolvas 10 elemet és rendezve írja
+ki őket!
+
+3. Fák! (Dalszöveg?)  Mélységi bejárás! Bináris keresés vagy rendezés.
+
+4. Duplán láncolt lista: van value, next, prev (azaz previous = előző).
+Ehhez a műveletek megírása: insert, delete, find.
+
+5. és 6. A fentiek közül valami, + előző anyagrészek (pl fájlok).
+Például, az első listába pakoljuk a páros számokat, második listába
+pakoljuk a páratlan számokat (a számokat egy fájlból olvassuk).
+
+}
