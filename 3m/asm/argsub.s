c270c126da3a82130fd5065f27d6cb844700543b
Emil Vatai <emil.vatai@gmail.com>
2015-02-23 12:10:33 +0100

    argumentum sorrend

--- 3m/asm/argsub.s
+++ 3m/asm/argsub.s
@@ -1,5 +1,7 @@
 	global 	argfun
 	section	.text
-argfun:	mov rax,rdi
-	add rax,rsi
+	;; az argumentumok sorban a következő regiszterekben
+	;; találhatók: RDI, RSI, RDX, RCX, R8, R9
+argfun:	mov rax,rdi		; rax = arg[0] 
+	add rax,rsi		; rax += arg[1]
 	ret
