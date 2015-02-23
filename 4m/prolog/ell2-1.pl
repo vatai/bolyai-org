5ac6e19b3c8328ff804b377dd44027e8002628b4
Emil Vatai <emil.vatai@gmail.com>
2015-02-23 15:14:47 +0100

    Ellenorzo elott.

--- 4m/prolog/ell2-1.pl
+++ 4m/prolog/ell2-1.pl
@@ -5,12 +5,20 @@
 % sima atom, plusz argumentum, {}, []
 
 % feladat
-s(X) --> valami(X),valamias.
+s(X) --> valami(X),valamimas.
 s --> [mas],valami,{p(X)}.
+s --> valami,[mas].
 
 % megoldás
 %s(X,A,C) :- valami(X,A,B),valamimas(B,C).
 %s([mas|A],B) :- valami(A,B),p(X).
+%s(A,B) :- valami(A,[mas|B]).
+
+aabb --> aa,bb.
+aa --> [a].
+bb --> [b].
+aabb(A,C) :- aa(A,B),bb(B,C).
+
 
 % 2. feladat
 % egyszerű nyelv, s --> np,vp stb.
@@ -27,3 +35,37 @@ s --> [mas],valami,{p(X)}.
 % 5. nem cfg nyelvek a^n b^n c^n
 
 % 6. parser tree (ld LPN) azaz bejárási fa.
+
+
+% Konkret pelda 2. es 3. feladatra.  
+
+% Mindegyik C program egy main fuggvenybol all, a main fuggveny
+% belsejeben utasitasok szerepelnek. az utolso utasitas egy return 0.
+% Utasitas lehet deklaracio, ertekadas. Valtozo nevek lehetnek 'x',
+% 'y' es 'z'.  Ertekek lehetnek valtozok vagy az 1, 2, 3 szamok. A
+% tipus lehet int, char vagy float.
+%
+% cprogi --> ['int main(){', utasitasok, 'return 0; }']
+% utasitasok --> utasitas.
+% utasitasok --> utasitas,utasitasok.
+% utasitas --> deklaracio.
+% utasitas --> ertekadas.
+% deklaracio --> tipus, valtozonev,[';'].
+% ertekadas --> valtozonev, ['='], ertek,[';'].
+% tipus --> [int].
+% tipus --> [char].
+% tipus --> [float].
+% valtozonev --> [x].
+% valtozonev --> [y].
+% valtozonev --> [z].
+% ertek --> valtozonev.
+% ertek --> szam.
+% szam --> ['1'].
+% szam --> ['2'].
+% szam --> ['3'].
+
+%int main(){
+%	int x;
+%	x = 5;
+%	return 0;
+%}
