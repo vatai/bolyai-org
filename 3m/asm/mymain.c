fe925ea944b08e1f7d4f19c1d00dea48b0083182
Emil Vatai <emil.vatai@gmail.com>
2015-02-23 11:08:06 +0100

    ciklus assemblyben

--- 3m/asm/mymain.c
+++ 3m/asm/mymain.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-  int a[]={13,42,69,88,3,14,23,3};
+  int a[]={13,42,69,88,3,14,23,4};
   int x;
   int n = sizeof(a)/sizeof(a[0]);
   int s;
@@ -10,8 +10,9 @@ int main(int argc, char *argv[])
   x = asd(4);
   printf("%d\n",x);
 
-  printf("sizeof(x)=%d\n",n);
-  // s = arraysum(a,n);
-  printf("arraysum() = %n\n",s);
+  printf("sizeof(a[])=%d\n",n);
+  printf("sizeof(a[0])=%d\n",sizeof(a[0]));
+  s = arraysum(a,n);
+  printf("arraysum() = %llx\n",s);
   return 0;
 }
--- 3m/asm/mysub.s
+++ 3m/asm/mysub.s
@@ -12,3 +12,15 @@ ciklus:	cmp rcx,rdi		; if !(i<=n)
 	inc rcx			; i++
 	jmp ciklus		; ismetles
 vege:	ret
+;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	global 	arraysum
+arraysum:
+	mov rax,0
+	mov rcx,0		; i=0
+loop1:	cmp rcx,rsi		; if !(i<n)
+	jnl end			; jmp -> vege
+	add rax,[rdi+4*rcx]	; rv += i
+	inc rcx			; i++
+	jmp loop1		; ismetles
+end:	ret
+	
