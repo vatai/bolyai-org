e22b5bf1c3c4f31fd02cfb370c1eef6a3d861cb7
Emil Vatai <emil.vatai@gmail.com>
2015-03-02 08:25:56 +0100

    Blanka delete_paros programja.

--- 2m/pascal/ell2-1.pas
+++ 2m/pascal/ell2-1.pas
@@ -19,3 +19,84 @@ Például, az első listába pakoljuk a páros számokat, második listába
 pakoljuk a páratlan számokat (a számokat egy fájlból olvassuk).
 
 }
+
+program delete_paros;
+type
+   node_t = record
+	       value : integer;
+	       next  : ^node_t;
+	    end;
+   list_t = ^node_t;
+
+procedure writelist(a : list_t);
+begin
+   while a <> nil do begin
+      writeln(a^.value);
+      a := a^.next;
+   end;
+end;
+
+procedure insert(var a : list_t; v : integer);
+var
+   tmp : list_t;
+begin
+   new(tmp);
+   tmp^.value := v;
+   tmp^.next := a;
+   a := tmp;
+end;
+
+procedure delete_paros(var x : list_t);
+var
+  a, tmp : list_t;
+begin
+   a:=x;
+
+if a<>nil then
+ begin
+
+   while a^.next<>nil do
+    begin
+       if (a^.next^.value mod 2) <> 0 then
+        begin
+          a:=a^.next;
+        end
+       else
+       begin
+        tmp:=a^.next^.next;
+        dispose(a^.next);
+        //new(a^.next);
+        a^.next:=tmp;
+        end;
+
+
+     if (x^.value mod 2)=0 then
+      begin
+      tmp:=x^.next;
+      dispose(x);
+      x:=tmp;
+      end;
+   end;
+
+ end;
+
+
+end;
+var
+   l1:list_t;
+begin
+  insert(l1,1);
+  insert(l1,2);
+  insert(l1,3);
+  insert(l1,4);
+  insert(l1,5);
+  insert(l1,6);
+  insert(l1,7);
+  writelist(l1);
+  writeln('--------------------------');
+
+  delete_paros(l1);
+  writelist(l1);
+
+  readln;
+end.
